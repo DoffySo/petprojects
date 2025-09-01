72f2c38aaf69814f673c9cbbd3529e954d7d7688
doffyso <bitvapesen@gmail.com>
2025-09-01 12:13:32 +0300

    form sketch

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import TodoList from '@/components/TodoList.tsx'
 function App() {
   return (
     <>
-      <TodoList id={1} name="Test" items={[{id:0, itemText: "Test", isFinished: true},{id:1, itemText: "Test2", isFinished: false}]} />
-      <TodoList id={2} name="Test2" />
+      <div className="container">
+        <div className="form">
+          <input className="form-input" type="text" placeholder="Todo List name..." />
+          <button className="form-submit" type="submit">Create</button>
+        </div>
+        <div className="todo-lists">
+          <TodoList id={1} name="Test" items={[{id:0, itemText: "Test", isFinished: true},{id:1, itemText: "Test2", isFinished: false}]} />
+          <TodoList id={2} name="Test2" />
+        </div>
+      </div>
     </>
   )
 }
